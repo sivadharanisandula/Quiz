c4c7ccc1c8174d8c4181fed76552116bba76768a
sivadharanisandula <154986893+sivadharanisandula@users.noreply.github.com>
2025-02-01 18:01:29 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import QuizApp from "./QuizApp";
+import "./index.css"; // Import Tailwind styles
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <QuizApp />
+  </React.StrictMode>
+);
